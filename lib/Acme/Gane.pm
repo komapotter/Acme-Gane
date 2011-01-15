0f5d0dd291533c539c4d02843f563876a03fae63
root <root@DeathStar.com>
2011-01-16 00:08:57 +0900

    initial commit

---
+++ lib/Acme/Gane.pm
@@ -0,0 +1,32 @@
+package Acme::Gane;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::Gane -
+
+=head1 SYNOPSIS
+
+  use Acme::Gane;
+
+=head1 DESCRIPTION
+
+Acme::Gane is
+
+=head1 AUTHOR
+
+komapotter E<lt>tora.05.036jp@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
